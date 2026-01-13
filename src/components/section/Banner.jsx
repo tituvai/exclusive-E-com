52783aaf2a7d79a4e16112e486622504fe41df82
tituvai <titu17940@gmail.com>
2026-01-13 13:16:27 +0600

    add slider

--- src/components/section/Banner.jsx
+++ src/components/section/Banner.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 
 
-import { Navigation, Pagination,  A11y } from 'swiper/modules';
+import { Navigation, } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -16,11 +17,15 @@ const Banner = () => {
     <>
       <Swiper
       // install Swiper modules
-      modules={[Navigation, Pagination, A11y]}
+      modules={[Navigation]}
       spaceBetween={50}
       slidesPerView={1}
-      navigation
+      navigation={{
+        nextEl: ".next_arrews",
+        prevEl: ".prive_arrews"
+      }}
       pagination={{ clickable: true }}
+      className='group'
     >
       <SwiperSlide>
          <Link to={'/'}><div className='bg-[url("/src/assets/banner01.png")] bg-no-repeat bg-cover bg-center py-30 w-full' >Benner</div></Link>
@@ -29,6 +34,12 @@ const Banner = () => {
          <Link to={'/'}><div className='bg-[url("/src/assets/banner01.png")] bg-no-repeat bg-cover bg-center py-30 w-full' >Benner</div></Link>
       </SwiperSlide>
       <SwiperSlide> <Link to={'/'}><div className='bg-[url("/src/assets/banner01.png")] bg-no-repeat bg-cover bg-center py-30 w-full' >Benner</div></Link></SwiperSlide>
+      <div className="next_arrews absolute top-1/2 left-1 -translate-y-1/2 transition-all  w-[30px] h-[30px] bg-white z-40 flex justify-center items-center rounded cursor-pointer opacity-0 group-hover:opacity-100 duration-[1s] -translate-x-4 group-hover:translate-x-0">
+        <FaLongArrowAltLeft/>
+      </div>
+      <div className="prive_arrews absolute top-1/2 right-1 -translate-y-1/2 transition-all  w-[30px] h-[30px] bg-white z-40 flex justify-center items-center rounded cursor-pointer opacity-0 group-hover:opacity-100 duration-[1s] translate-x-4 group-hover:-translate-x-0">
+        <FaLongArrowAltRight/>
+      </div>
     </Swiper>
    
 
