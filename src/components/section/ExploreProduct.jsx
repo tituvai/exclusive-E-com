06e5cea89f28b5dbef525deacd8e4ef85e0538d7
tituvai <titu17940@gmail.com>
2026-01-14 13:49:37 +0600

    add model

--- src/components/section/ExploreProduct.jsx
+++ src/components/section/ExploreProduct.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Container from '../common/Container'
 import BgrTitle from '../common/BgrTitle'
 import Flex from '../common/Flex'
@@ -7,31 +7,62 @@ import { FaArrowLeftLong, FaArrowRightLong} from "react-icons/fa6";
 import Product from './Product'
 import Button from '../common/Button'
 
+// Import Swiper styles
+import { Navigation, } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
 
 const ExploreProduct = () => {
+
+// const prevRef = useRef(null);
+// const nextRef = useRef(null);
   return (
    <>
    <div className="py-10">
     <Container>
         <div className="">
             <BgrTitle bgrText={'Our Products'}/>
-            <Flex className={'pt-5'}>
-                <MainTitle mainText={'Explore Our Products'}/>
-                <div className="flex items-center gap-x-2">
-                    <div className="bg-productC w-[30px] h-[30px] rounded-full flex justify-center items-center"><FaArrowLeftLong/></div>
-                    <div className="bg-productC w-[30px] h-[30px] rounded-full flex justify-center items-center"><FaArrowRightLong/></div>
-                </div>
-            </Flex>
-            <Flex className={'flex-wrap gap-y-18 pt-15'}>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-            </Flex>
+            <MainTitle className={'pb-10 pt-5'} mainText={'Explore Our Products'}/>
+          {/* install Swiper modules */}
+     <div className="relative">
+        <div className="absolute -top-30 right-13 z-40">
+        <div className="explore-prev bg-productC w-[30px] h-[30px] rounded-full flex justify-center items-center absolute top-10 right-13 z-40 cursor-pointer"><FaArrowLeftLong/></div>
+        <div  className="explore-next bg-productC w-[30px] h-[30px] rounded-full flex justify-center items-center absolute top-10 right-1 z-40 cursor-pointer"><FaArrowRightLong/></div>
+    </div>
+        <Swiper
+      modules={[Navigation]}
+      spaceBetween={50}
+      slidesPerView={4}
+      navigation={{
+      prevEl: '.explore-prev',
+      nextEl: '.explore-next',
+    }}     
+      pagination={{ clickable: true }} 
+      className="overflow-hidden"
+    >
+      <SwiperSlide>
+        <Product/>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Product/>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Product/>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Product/>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Product/>
+      </SwiperSlide>
+    
+    
+    </Swiper>
+     </div>
             <div className="flex justify-center pt-25">
                 <Button  btnText={'View All Products'}/>
             </div>
--- src/components/section/Product.jsx
+++ src/components/section/Product.jsx
@@ -7,6 +7,18 @@ import { Link } from 'react-router-dom';
 import Title from '../common/Title';
 import { IoStar } from "react-icons/io5";
 
+import Flex from '../common/Flex'
+import game from '/src/assets/gameing.png'
+import game_1 from '/src/assets/game_1.png'
+import game_2 from '/src/assets/game_2.png'
+import game_3 from '/src/assets/game_3.png'
+import game_4 from '/src/assets/game_4.png'
+import { IoIosStar } from "react-icons/io";
+import car from '/src/assets/cor.png'
+import retur from '/src/assets/return.png'
+import { FaRegHeart } from "react-icons/fa6";
+import { MdOutlineClear } from "react-icons/md";
+
 // shadcn ui import Part Start 
 import {
   AlertDialog,
@@ -20,6 +32,7 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
+import Peragrap from '../common/Peragrap';
 // shadcn ui import Part End
 const Product = () => {
   return (
@@ -38,15 +51,87 @@ const Product = () => {
             </AlertDialogTrigger>
             <AlertDialogContent>
               <AlertDialogHeader>
-                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
                 <AlertDialogDescription>
-                  This action cannot be undone. This will permanently delete your
-                  account and remove your data from our servers.
+                  {/* Main Containe Start  */}
+                  <div className={'pt-10'}>
+                <div className=" flex justify-between">
+                    <div className="w-[25%] flex flex-col gap-y-2">
+                        <picture>
+                            <Image className={'w-full h-[70px]'} imgSrc={game_1} imgAlt={"gameing.png"}/>
+                        </picture>
+                        <picture>
+                            <Image className={'w-full h-[70px]'} imgSrc={game_2} imgAlt={"gameing.png"}/>
+                        </picture>
+                        <picture>
+                            <Image className={'w-full h-[70px]'} imgSrc={game_3} imgAlt={"gameing.png"}/>
+                        </picture>
+                        <picture>
+                            <Image className={'w-full h-[70px]'} imgSrc={game_4} imgAlt={"gameing.png"}/>
+                        </picture>
+                    </div>
+                    <div className="w-[70%]">
+                        <picture>
+                            <Image className={'w-full h-[300px]'} imgSrc={game} imgAlt={"gameing.png"}/>
+                        </picture>
+                    </div>
+                </div>
+                <div className="">
+                    <Title className={'text-xl text-black font-inter font-semibold leading-6 pt-2'} text={'Havic HV G-92 Gamepad'} as={'h4'}/>
+                    <div className="flex items-center gap-x-5 pt-2">
+                      <Title className={'text-xl text-black font-inter leading-6 py-3'} text={'$192.00'} as={'h4'}/>
+                        <div className="flex items-center gap-x-1">
+                            <IoIosStar className='size-4 text-yellow-500'/>
+                            <IoIosStar className='size-4 text-yellow-500'/>
+                            <IoIosStar className='size-4 text-yellow-500'/>
+                            <IoIosStar className='size-4 text-yellow-500'/>
+                            <IoIosStar className='size-4 text-yellow-500'/>
+                            <span className='text-gray-500 text-sm font-inter'>(123 reviewS)</span>
+                        </div>
+                        <div className="w-[2px] h-[20px] bg-gray-500"></div>
+                        <div className="flex items-center">
+                            <Title className={'text-sm text-offerC font-poppin leading-5'} text={'In Stock'} as={'h6'}/>
+                            <span className='text-sm text-offerC font-poppin font-semibold'>(34)</span>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                      <div className="flex items-center gap-x-5 py-3">
+                        <Title className={'text-lg text-black font-inter leading-5'} text={'Colours :'} as={'h6'}/>
+                        <div className="flex items-center gap-x-1">
+                            <div className="w-[20px] h-[20px] rounded-full bg-gray-500"></div>
+                            <div className="w-[20px] h-[20px] rounded-full bg-primery"></div>
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-x-5">
+                        <Title className={'text-lg text-black font-inter leading-5'} text={'Size :'} as={'h6'}/>
+                        <div className="flex items-center gap-x-2">
+                            <Title className={'w-[30px] text-sm text-black font-medium font-poppin leading-5 p-1 text-center border-2 rounded border-borderC cursor-pointer hover:bg-primery hover:text-white transition-all duration-[1s] uppercase'} text={'xs'} as={'h6'}/>
+                            <Title className={'w-[30px] text-sm text-black font-medium font-poppin leading-5 p-1 text-center border-2 rounded border-borderC cursor-pointer hover:bg-primery hover:text-white transition-all duration-[1s] uppercase'} text={'s'} as={'h6'}/>
+                            <Title className={'w-[30px] text-sm text-black font-medium font-poppin leading-5 p-1 text-center border-2 rounded border-borderC cursor-pointer hover:bg-primery hover:text-white transition-all duration-[1s] uppercase'} text={'m'} as={'h6'}/>
+                            <Title className={'w-[30px] text-sm text-black font-medium font-poppin leading-5 p-1 text-center border-2 rounded border-borderC cursor-pointer hover:bg-primery hover:text-white transition-all duration-[1s] uppercase'} text={'l'} as={'h6'}/>
+                            <Title className={'w-[30px] text-sm text-black font-medium font-poppin leading-5 p-1 text-center border-2 rounded border-borderC cursor-pointer hover:bg-primery hover:text-white transition-all duration-[1s] uppercase'} text={'xl'} as={'h6'}/>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className=" flex items-center gap-x-7 pt-3">
+                        <div className="flex items-center justify-between w-[100px] border-2  border-borderC rounded">
+                            <span className='font-bold text-base border-r-2 border-borderC px-3 hover:bg-primery hover:text-white cursor-pointer py-1 flex justify-center items-center w-[30px]'>-</span>
+                            <span>1</span>
+                            <span className='font-bold text-base border-l-2 border-borderC px-3 hover:bg-primery hover:text-white cursor-pointer py-1 flex justify-center items-center w-[30px]'>+</span>
+                        </div>
+                        <div className="flex items-center gap-x-3">
+                            <Button className={'cursor-pointer'}>Buy Now</Button>
+                            <div className="w-[30px] flex justify-center items-center rounded border-2 border-borderC py-1"><FaRegHeart className='hover:text-primery size-4'/></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+                  {/* Main Containe End */}
                 </AlertDialogDescription>
               </AlertDialogHeader>
               <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction>Continue</AlertDialogAction>
+                <AlertDialogCancel className={'absolute top-2 right-2 cursor-pointer'}><MdOutlineClear/></AlertDialogCancel>
               </AlertDialogFooter>
             </AlertDialogContent>
           </AlertDialog>
@@ -70,7 +155,6 @@ const Product = () => {
             <span className='text-sm text-gray-500 font-semibold'>(34)</span>
           </div>
         </div>
-    {/* quick view Part Start  */}
     </div>
 
   )
